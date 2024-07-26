c89662b10c9f83c650d802c807dac40c52a45fe0
Pinninti Pavan <pinnintipavan9989@gmail.com>
2024-07-26 22:09:03 +0530

    new link

--- movie-gold-v1-master/src/api/axiosConfig.js
+++ movie-gold-v1-master/src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 //just checking
-const baseURL = 'http://localhost:8080'; // New base URL
+const baseURL = 'https://manga-movies9.netlify.app/'; // New base URL
 
 const api = axios.create({
     baseURL: baseURL,
