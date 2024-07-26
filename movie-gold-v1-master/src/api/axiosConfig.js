62df0cf42447ac1e5899f014ac44a870d329eaab
Pinninti Pavan <pinnintipavan9989@gmail.com>
2024-07-26 23:43:19 +0530

    new

--- movie-gold-v1-master/src/api/axiosConfig.js
+++ movie-gold-v1-master/src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-//just checking
-const baseURL = 'https://manga-movies9.netlify.app/'; // New base URL
+// just checking
+const baseURL = 'https://glittery-crepe-1d6589.netlify.app/'; // New base URL
 
 const api = axios.create({
     baseURL: baseURL,
