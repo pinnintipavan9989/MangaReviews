374e99b92bd845b99dfe6762b8a97f001ad5fd6f
Pinninti Pavan <pinnintipavan9989@gmail.com>
2024-07-27 17:58:40 +0530

    frontend is done

--- Manga-Movies/src/main/java/pavan/pinninti/movieist/CorsConfig.java
+++ Manga-Movies/src/main/java/pavan/pinninti/movieist/CorsConfig.java
@@ -10,7 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+//                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://66a4e0a9b95c2e0792c987ec--superlative-parfait-254648.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
