44ac83b0e0f26b093e05df111c0bc58b776e795a
pinnintipavan9989 <pinnintipavan9989@gmail.com>
2024-07-29 01:55:10 +0530

    new 408

--- Manga-Movies/src/main/java/pavan/pinninti/movieist/CorsConfig.java
+++ Manga-Movies/src/main/java/pavan/pinninti/movieist/CorsConfig.java
@@ -10,9 +10,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-//                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000")
 //                .allowedOrigins("https://superlative-parfait-254648.netlify.app")
-                .allowedOrigins("https://66a4e0a9b95c2e0792c987ec--superlative-parfait-254648.netlify.app")
+//                .allowedOrigins("https://66a4e0a9b95c2e0792c987ec--superlative-parfait-254648.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
